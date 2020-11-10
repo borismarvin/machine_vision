4f1dd801a43e50e55cae9954869be4663b996054
borismarvin <51974514+borismarvin@users.noreply.github.com>
2020-11-10 14:58:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,9 @@
 #include "AVL.h"
 #include <QPixmap>
 
-using namespace std;
-using namespace avl;
-using namespace atl;
 
 int main(int argc, char *argv[])
 {
-
-    //"/home/Downloads/road lines.jpg"
-
-
-
-
 
     QApplication a(argc, argv);
     MainWindow w;
